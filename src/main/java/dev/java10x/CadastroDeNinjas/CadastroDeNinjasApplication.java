3d2ddf6708d298d4945d39d9f1a574f4423d2483
cesarmazzaro <cesarmazzaro@gmail.com>
2025-07-04 01:04:33 -0300

    Primeira rota para inicializar o spring boot na web via tomcat

--- src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -8,7 +8,6 @@ public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
-		System.out.println("Hello, World!");
 	}
 
 }
