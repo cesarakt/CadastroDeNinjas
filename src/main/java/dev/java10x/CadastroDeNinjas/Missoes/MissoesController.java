27ff6cabecfb8ada876309cfa61acfb81695b9c6
Cesar Mazzaro <cesarmazzaro@gmail.com>
2025-07-07 21:02:44 -0300

    feat: Adicionando o mock de endpoints no controller de missoes

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,33 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    @GetMapping("/missoes")
+    public String getMissoes () {
+        return "todas as missões";
+    }
+
+    @GetMapping("/missoesID")
+    public String getMissoesPorId () {
+        return "Missão por ID";
+    }
+
+    @PostMapping("/criar")
+    public String criarMissao () {
+        return "Missão criada com sucesso.";
+    }
+
+    @PutMapping("/alterar")
+    public String alterarMissao () {
+        return "Missão alterada com sucesso.";
+    }
+
+    @DeleteMapping("/deletar")
+    public String deletarMissaoPorId () {
+        return "missão deletada.";
+    }
+
 }
